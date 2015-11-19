38fcbe9cdf87e96b63f3831e6baee385d4333e44
Robert Brotherus <robert@iki.fi>
2015-11-19 22:25:54 +0200

    xml.clj for xml text output

---
+++ xml.clj
@@ -0,0 +1,31 @@
+(ns Xml
+  (:require [clojure.string :as str])
+  (:use Utils)
+  (:use clojure.test))
+
+;----------------- xml to text string -------------------------
+
+(defn- key-to-str [ key ]
+  (cond
+    (string? key) key
+    (keyword? key) (name key)
+    :else (pr-str key)))
+
+(defn- attr-to-str [ [ key value ] ]
+  (str (key-to-str key) "=\"" value "\"" ))
+
+(defn- attrs-to-str [ attrs ]
+  (if (empty? attrs) ""
+    (str " " (str/join " " (map attr-to-str attrs)))))
+
+(def xml-to-text)
+
+(defn- content-to-str [ content ind ]
+  (str/join "" (map #(xml-to-text % ind) content)))
+
+(defn xml-to-text
+  ( [ element ] (xml-to-text element 0) )
+  ( [ [ tag attrs & content ] indent ]
+    (str (indent-str indent) "<" (name tag) (attrs-to-str attrs)
+         (if (empty? content) "/>"
+           (str ">" (content-to-str content (inc indent)) (indent-str indent) "</" (name tag) ">" )))))
