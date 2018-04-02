02124a9ddbe1fba510b63c16d21fdb0cb3a7f306
Robert Brotherus <robert@iki.fi>
2018-04-02 09:31:45 +0300

    project.clj :deploy-repositories for "lein deploy clojars" :-)

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
-(defproject clojure-common "0.1.0"
-  :description "Robert Brotherus Common Clojure routines"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.rbrother/clojure-common "0.1.3"
+  :description "Robert J. Brotherus Common Clojure routines"
+  :url "https://github.com/rbrother/clojure-common"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :deploy-repositories [["clojars"
+                         { :sign-releases false
+                           :url "https://clojars.org/repo"
+                          :username "rbrother"
+                          :password "manga5zelda" }]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]])
